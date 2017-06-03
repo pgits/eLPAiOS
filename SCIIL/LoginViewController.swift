bc9540987b9e82393f0bc6e0ff3bb2d7dc31820a
Peter Gits <pgits@geekgaps.com>
2017-06-02 20:28:25 -0400

    Added Siren to this project.  Not currently getting a view to show up

--- SCIIL/LoginViewController.swift
+++ SCIIL/LoginViewController.swift
@@ -85,6 +85,8 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                 self.performSegue(withIdentifier: "AuditView", sender: nil)
             }
         }
+        //setupSiren()
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -265,5 +267,72 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         //       Use your own number attributes to track median value over time
         Answers.logCustomEvent(withName: title, customAttributes: [key:value])
     }
-
+    
+    func setupSiren() {
+        let siren = Siren.sharedInstance
+        
+        // Optional
+        siren.delegate = self
+        
+        // Optional
+        siren.debugEnabled = true
+        
+        // Optional
+        siren.appName = "SCIIL eLPA"
+        
+        // Optional - Defaults to .Option
+        //        siren.alertType = .Option // or .Force, .Skip, .None
+        
+        // Optional - Can set differentiated Alerts for Major, Minor, Patch, and Revision Updates (Must be called AFTER siren.alertType, if you are using siren.alertType)
+        siren.majorUpdateAlertType = .option
+        siren.minorUpdateAlertType = .option
+        siren.patchUpdateAlertType = .option
+        siren.revisionUpdateAlertType = .option
+        
+        // Optional - Sets all messages to appear in Spanish. Siren supports many other languages, not just English and Russian.
+        //        siren.forceLanguageLocalization = .Russian
+        
+        // Optional - Set this variable if your app is not available in the U.S. App Store. List of codes: https://developer.apple.com/library/content/documentation/LanguagesUtilities/Conceptual/iTunesConnect_Guide/Appendices/AppStoreTerritories.html
+        //        siren.countryCode = ""
+        
+        // Optional - Set this variable if you would only like to show an alert if your app has been available on the store for a few days. The number 5 is used as an example.
+        //        siren.showAlertAfterCurrentVersionHasBeenReleasedForDays = 5
+        
+        // Required
+        siren.checkVersion(checkType: .immediately)
+    }
 }
+
+
+extension LoginViewController: SirenDelegate
+{
+    func sirenDidShowUpdateDialog(alertType: SirenAlertType) {
+        print(#function, alertType)
+    }
+    
+    func sirenUserDidCancel() {
+        print(#function)
+    }
+    
+    func sirenUserDidSkipVersion() {
+        print(#function)
+    }
+    
+    func sirenUserDidLaunchAppStore() {
+        print(#function)
+    }
+    
+    func sirenDidFailVersionCheck(error: NSError) {
+        print(#function, error)
+    }
+    
+    func sirenLatestVersionInstalled() {
+        print(#function, "Latest version of app is installed")
+    }
+    
+    // This delegate method is only hit when alertType is initialized to .none
+    func sirenDidDetectNewVersionWithoutAlert(message: String) {
+        print(#function, "\(message)")
+    }
+}
+
